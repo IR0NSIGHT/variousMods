d200b166be8de1ba08b65efd19283f5ccc407049
irnsght <irnsght@gmail.com>
2021-12-31 13:35:12 +0100

    revert event to be single code in NPCControl

--- NPCControl/src/me/iron/npccontrol/triggers/Event.java
+++ NPCControl/src/me/iron/npccontrol/triggers/Event.java
@@ -3,11 +3,7 @@ package me.iron.npccontrol.triggers;
 import me.iron.npccontrol.ModMain;
 import org.luaj.vm2.ast.Str;
 import org.lwjgl.Sys;
-import org.lwjgl.openal.Util;
 import org.schema.common.util.linAlg.Vector3i;
-
-import java.util.HashMap;
-import java.util.Set;
 
 /**
  * STARMADE MOD
@@ -16,35 +12,16 @@ import java.util.Set;
  * TIME: 21:30
  */
 public class Event {
-    final private HashMap<Long,Long> code_to_last = new HashMap<>();
+    private long code;
+    private long last;
+    private int cooldown;
+    private int chance;
 
-    /**
-     *
-     */
-    public Event() {
+    public Event(long code, int chance, int cooldown) {
+        this.code = code;
+        this.chance = chance;
+        this.cooldown = cooldown;
     };
-
-    public void setCode(long code) {
-        code_to_last.put(code,-1L);
-    }
-
-    public boolean removeCode(long code) {
-        if (code_to_last.containsKey(code)) {
-            code_to_last.remove(code);
-            return true;
-        }
-        return false;
-    }
-
-    public Set<Long> getCodes() {
-        return code_to_last.keySet();
-    }
-
-    private long getLastByCode(long code) {
-        if (!code_to_last.containsKey(code))
-            return -1L;
-        return code_to_last.get(code);
-    }
 
     /**
      * method that gets called when the event is triggered. will evaluate cooldown and random chance.
@@ -61,25 +38,24 @@ public class Event {
      * @return
      */
     protected boolean triggerCheck(long inputCode, Vector3i pos) {
-       if (Utility.getRand().nextInt(255) >= Utility.get8bit(inputCode,7)) {
-           //ModMain.log("abort random chance");
-           return false;
-       }
-       long cooldown = Utility.get8bit(inputCode,6);
-        if (cooldown > 0 && System.currentTimeMillis()< getLastByCode(c) +cooldown) {
-            //ModMain.log("abort cooldown, wait: " + (last+cooldown- System.currentTimeMillis()));
+        if (Math.random()*100>=chance) {
+            ModMain.log("abort random chance");
             return false;
         }
-        //ModMain.log(String.format("%s input\n%s code\n%s differece\n\n",Trigger.toBin(inputCode),Trigger.toBin(code),Trigger.toBin(inputCode&code)));
+        if (cooldown > 0 && System.currentTimeMillis()<last+cooldown) {
+            ModMain.log("abort cooldown, wait: " + (last+cooldown- System.currentTimeMillis()));
+            return false;
+        }
+        ModMain.log(String.format("%s input\n%s code\n%s differece\n\n",Trigger.toBin(inputCode),Trigger.toBin(code),Trigger.toBin(inputCode&code)));
         for (long i = 0; i < 8; i++) {
             long mask =255L<<(8L*i);
-            ////ModMain.log(String.format("%s mask, %s value",Trigger.toBin(mask),mask));
-        //    //ModMain.log(String.format("%s mask \n%s input\n%s code\n%s differece\n\n",Trigger.toBin(mask),Trigger.toBin(mask&inputCode),Trigger.toBin(mask&code),Trigger.toBin((mask&(inputCode&code)))));
+            //ModMain.log(String.format("%s mask, %s value",Trigger.toBin(mask),mask));
+            ModMain.log(String.format("%s mask \n%s input\n%s code\n%s differece\n\n",Trigger.toBin(mask),Trigger.toBin(mask&inputCode),Trigger.toBin(mask&code),Trigger.toBin((mask&(inputCode&code)))));
 
-      //     if (((mask&code) != 0) && (mask&(inputCode&code)) == 0) {
-      //         //ModMain.log("ABORT code mismatch");
-      //         return false;
-      //     }
+            if (((mask&code) != 0) && (mask&(inputCode&code)) == 0) {
+                ModMain.log("ABORT");
+                return false;
+            }
         }
         return true;
     }
@@ -88,27 +64,11 @@ public class Event {
      * internal method that runs when the event successfully triggers
      */
     protected void run(long code, Vector3i sector) {
-        //ModMain.log(String.format("event %s was run with c=%s, pos=%s",getName(),Long.toBinaryString(code),sector.toStringPure()));
-      //  last = System.currentTimeMillis();
+        ModMain.log(String.format("event %s was run with c=%s, pos=%s",getName(),Long.toBinaryString(code),sector.toStringPure()));
+        last = System.currentTimeMillis();
     }
 
     protected String getName() {
         return "?";
-    }
-
-    public static void main(String[] args) {
-        long code = 0;
-        code = Utility.add8bit(code,0b00001111,7);
-        code = Utility.add8bit(code,0b00001111,1);
-
-        System.out.println(Trigger.toBin(code));
-        code = Utility.add8bit(code,0b10101010,1);
-
-        System.out.println(Trigger.toBin(code));
-        System.out.println(Utility.get8bit(code,7));
-        System.out.println(Utility.get8bit(code,1));
-
-      //  System.out.println(Integer.toBinaryString(Utility.get8bit(code,1)));
-
     }
 }
